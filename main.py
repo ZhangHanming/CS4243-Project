d3b55f80c11c3eea5e78897bbd760fcff8ee2bec
ZhangHanming <zhang.hanming.official@gmail.com>
2017-10-20 12:54:34 +0800

    test videocapture

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import numpy as np
-import cv2 as cv2
+import cv2
 
+cap = cv2.VideoCapture("Videos/traffic.mp4")
 
+while(True):
+    ret, frame = cap.read()
+
+    if(not ret):
+        break
+
+    print frame.shape
