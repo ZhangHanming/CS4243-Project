4bd98d9102a29d37caf4cd2871ace80ccb4dc701
Zhang Hanming <zhang.hanming.official@gmail.com>
2017-10-20 22:28:14 +0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CS4243-Project
 
 Python Version: 2.7.12
+OpenCV Version: 3.3.0
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture("Videos/traffic.mp4")
+cap = cv2.VideoCapture("./Videos/traffic.mp4")
 
 while(True):
     ret, frame = cap.read()
