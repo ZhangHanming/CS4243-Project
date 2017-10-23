04a6e2f0bc25046f0c0b64713f883ad5b0e96f87
Zhang Hanming <zhang.hanming.official@gmail.com>
2017-10-24 00:16:17 +0800

    add key pressed break

--- main.py
+++ main.py
@@ -26,6 +26,11 @@ while(1):
     r, c = trackFeatures(old_frame_gray, new_frame_gray, r, c)
     cv2.imshow('frame', markWithCircle(r, c, new_frame))
 
+    # break when ESC is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
     old_frame = new_frame
     old_frame_gray = new_frame_gray
 
