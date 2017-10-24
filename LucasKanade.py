087e04f533300748967b96739e1807ad84549e1f
Zhang Hanming <zhang.hanming.official@gmail.com>
2017-10-24 19:42:54 +0800

    add downsample method

--- LucasKanade.py
+++ LucasKanade.py
@@ -2,4 +2,13 @@ import numpy as np
 import cv2
 
 def trackFeatures(old_frame, new_frame, r, c):
-    #TODO
+    return None
+
+
+def downsample(frame):
+    new_frame = np.zeros(map(lambda x: x/2, frame.shape))
+    for i in range(0,frame.shape[0],2):
+        for j in range(0,frame.shape[1],2):
+            new_frame[i/2][j/2] = frame[i:i+2,j:j+2].sum() / 4
+
+    return new_frame
