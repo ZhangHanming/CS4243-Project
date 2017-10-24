9fa45d6b28b20560f7f35370219e288cfcc29037
Zhang Hanming <zhang.hanming.official@gmail.com>
2017-10-24 19:51:11 +0800

    add create pyramid method

--- LucasKanade.py
+++ LucasKanade.py
@@ -4,6 +4,16 @@ import cv2
 def trackFeatures(old_frame, new_frame, r, c):
     return None
 
+def createPyramid(frame, minSize = 32):
+    pyramid = [frame]
+    while(frame.shape[0] > minSize and frame.shape[1] > minSize):
+        sample = downsample(frame)
+        pyramid = [sample] + pyramid
+        frame = sample
+
+    return pyramid
+
+
 
 def downsample(frame):
     new_frame = np.zeros(map(lambda x: x/2, frame.shape))
@@ -12,3 +22,5 @@ def downsample(frame):
             new_frame[i/2][j/2] = frame[i:i+2,j:j+2].sum() / 4
 
     return new_frame
+
+
