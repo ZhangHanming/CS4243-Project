9b27edd76f44bae1c984c00f76e1103eef321863
Sheng Xuan <shengxuan14@gmail.com>
2017-10-25 18:13:40 +0800

    Draw smaller circle

--- Draw.py
+++ Draw.py
@@ -1,7 +1,8 @@
 import cv2
 
+
 def markWithCircle(r, c, frame):
     for i in range(len(r)):
-        frame = cv2.circle(frame, (c[i], r[i]), 5, (0, 0, 255), 1)
+        frame = cv2.circle(frame, (c[i], r[i]), 2, (0, 0, 255), 2)
 
     return frame
