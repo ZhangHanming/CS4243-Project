b9d9489bca46013dc4c618bbe5550b12fa706cd5
Sheng Xuan <shengxuan14@gmail.com>
2017-11-17 12:29:20 +0800

    Clean up and add comments

--- addImage.py
+++ addImage.py
@@ -3,6 +3,19 @@ import cv2
 
 
 def addImage(row, col, angle, scale, size, img, bg):
+    """Funtion to paste an image onto a background image
+    Args:
+        row: the row number of center to be pasted on the background
+        col: the col number of center to be pasted on the background
+        angle: the anti-clockwise angle of the image (in degree)
+        scale: the resizing scale of the image
+        img: image to be pasted
+        bg: backgroung image
+
+    Returns:
+        bg: finshed image
+
+    """
     row = int(row)
     col = int(col)
     img_width = img.shape[1]
@@ -15,6 +28,7 @@ def addImage(row, col, angle, scale, size, img, bg):
     M[1, 2] += int(scale * img_height)
     rotated_img = cv2.warpAffine(img, M, (size, size))
     half_size = size / 2
+    # Set the region of interest for image and background
     img_ROI_row1 = 0
     img_ROI_row2 = size
     img_ROI_col1 = 0
@@ -24,7 +38,7 @@ def addImage(row, col, angle, scale, size, img, bg):
     bg_ROI_col1 = col - half_size
     bg_ROI_col2 = bg_ROI_col1 + size
 
-    # Handle exceed boundary cases
+    # Handle exceeding boundary cases
     if(bg_ROI_row1 < 0):
         img_ROI_row1 = -bg_ROI_row1
         bg_ROI_row1 = 0
@@ -45,21 +59,17 @@ def addImage(row, col, angle, scale, size, img, bg):
 
     img_ROI = rotated_img[img_ROI_row1:img_ROI_row2, img_ROI_col1:img_ROI_col2]
     img2Gray = cv2.cvtColor(img_ROI, cv2.COLOR_BGR2GRAY)
+
+    # Create a mask to cut the black part of the image
     ret, mask = cv2.threshold(img2Gray, 60, 255, cv2.THRESH_BINARY)
+    # Create a mask to clean the roi on the background
     mask_inv = cv2.bitwise_not(mask)
+
+    # Paste images by cv2.add
     img_ROI = cv2.bitwise_and(img_ROI, img_ROI, mask=mask)
     bg_ROI = bg[bg_ROI_row1:bg_ROI_row2, bg_ROI_col1:bg_ROI_col2]
     bg_ROI = cv2.bitwise_and(bg_ROI, bg_ROI, mask = mask_inv)
-    #print "___________________"
-    #print "size", size
-    #print "pos r c", row,col
-    #print "img row", img_ROI_row1,img_ROI_row2
-    #print "img col", img_ROI_col1,img_ROI_col2
-    #print "bg row", bg_ROI_row1,bg_ROI_row2
-    #print "bg col", bg_ROI_col1,bg_ROI_col2
-    #print "bg", bg_ROI.shape
-    #print "img", img_ROI.shape
     overlay = cv2.add(bg_ROI, img_ROI)
     bg[bg_ROI_row1:bg_ROI_row2, bg_ROI_col1:bg_ROI_col2] = overlay
-    #print bg[bg_ROI_row1:bg_ROI_row2, bg_ROI_col1:bg_ROI_col2].shape
+
     return bg
--- wings.py
+++ wings.py
@@ -8,10 +8,9 @@ imagePath = "Images/feather.jpg"
 cols = np.loadtxt("cols.csv", delimiter=",")
 rows = np.loadtxt("rows.csv", delimiter=",")
 
-# cols[pointNumber,frameNumber]
+# Expecting format: cols[pointNumber,frameNumber]
 
 corner_number = cols.shape[0]
-
 cap = cv2.VideoCapture(vPath)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -21,25 +20,36 @@ out = cv2.VideoWriter('Videos/wing.avi', fourcc, fps, (width, height))
 
 image = cv2.imread(imagePath, 1)
 rotate_mat_size = int(image.shape[0] * 2)
+
+# Read all frames and add image on each frame
 while(1):
     ret, frame = cap.read()
     frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
     if(not ret or frame_number == int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
         break
+
+    # interate all corners' coordinates in one frame
     for i in range(0, corner_number):
+        # r1: old row, r2: new row
+        # c1: old col, c2: new col
         r1 = rows[i, frame_number-1]
         r2 = rows[i, frame_number]
         c1 = cols[i, frame_number-1]
         c2 = cols[i, frame_number]
+        # Filter out the points in the body area, magic numbers are mearsured by hand
         if((c2 > 250 and c2 < 380) or (c1 > 250 and c2 < 380) or r1 > 340 or r2 > 340):
             continue
         distance = np.sqrt((r2 - r1)**2 + (c2 - c1)**2)
+        # Only draw points thats moves, and ignore noise that moves rapidly
         if(distance > 1.5 and distance < 13):
+            # give a random color for each feather (grey scale)
             random_grey_scale = np.random.uniform(0.5,1.0)
             new_image = (image * random_grey_scale).astype(np.uint8)
             angle = np.rad2deg(np.arctan2(c2 - c1, r2 - r1)) + 180
+            # Make sure the feature is not so big
             scale = distance / 5.0
             new_rotate_mat_size = int(scale * rotate_mat_size)
+            # Produce the new frame
             frame = addImage(r2, c2, angle, scale, new_rotate_mat_size, new_image, frame)
     out.write(frame)
 cap.release()
